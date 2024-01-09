55b73f1e8b6835d14d42d246c82439853de1a28a
Trainmaster2 <dphammock@gmail.com>
2024-01-08 18:24:41 -0800

    Created Vitis project

---
+++ midi-genie/software/midi_genie/src/main.cpp
@@ -0,0 +1,11 @@
+#include "xparameters.h"
+#include "xil_types.h"
+#include "xil_printf.h"
+
+// Get device IDs from xparameters.h
+
+int main() {
+	xil_printf("Hello, world!\r\n");
+
+	while (true) {}
+}
---
+++ midi-genie/software/midi_genie_hw/platform.tcl
@@ -0,0 +1,20 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct /home/trainmaster2/Documents/midi-genie/midi-genie/software/midi_genie_hw/platform.tcl
+# 
+# OR launch xsct and run below command.
+# source /home/trainmaster2/Documents/midi-genie/midi-genie/software/midi_genie_hw/platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {midi_genie_hw}\
+-hw {/home/trainmaster2/Documents/midi-genie/midi-genie/design_1_wrapper.xsa}\
+-out {/home/trainmaster2/Documents/midi-genie/midi-genie/software}
+
+platform write
+domain create -name {standalone_microblaze_0} -display-name {standalone_microblaze_0} -os {standalone} -proc {microblaze_0} -runtime {cpp} -arch {32-bit} -support-app {empty_application}
+platform generate -domains 
+platform active {midi_genie_hw}
+platform generate -quick
+platform generate
